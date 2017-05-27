70466e1fe92dfc0a9fc88b49b1addac498c1f33e
Ellis Squires <ellis.squires@gmail.com>
2017-05-27 11:45:41 +0100

    Added socket.io

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function() {
+  console.log("Someone joined");
 });
 
 http.listen(3000, function() {
