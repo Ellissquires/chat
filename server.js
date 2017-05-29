d23a7e8aa9f8c099db30211f86cb06050015fffc
Ellis Squires <ellis.squires@gmail.com>
2017-05-29 12:08:12 +0100

    Added chat message on disconnet

--- server.js
+++ server.js
@@ -3,20 +3,29 @@ var http = require('http').Server(app);
 
 var io = require('socket.io')(http);
 
-
+var user
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
-  var user = Date.now();
+  user = Date.now();
 
   socket.on('message.sent', function(message){
     io.emit('message', user + ': ' + message);
   });
+
   io.emit('message', 'User ' + user + ' has connected');
+
+
+  socket.on('disconnect', function () {
+    io.emit('message',user + ' has disconnected');
+  });
 });
+
+
+
 
 http.listen(process.env.PORT || 3000, function() {
   console.log("Started server");
